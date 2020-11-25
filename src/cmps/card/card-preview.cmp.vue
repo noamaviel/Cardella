306eb012d7373fbf0fd291f05817e2778a660a39
Mor Lev Ari <morshva@gmail.com>
2020-11-25 17:46:28 +0200

    comitush

---
+++ src/cmps/card/card-preview.cmp.vue
@@ -0,0 +1,16 @@
+<template>
+    <section class="card-preview-container" style="display:inline-block; padding:10px; margin:5px; border:1px solid black; width:200px; height:400px">
+        <h1>card</h1>
+        {{card.title}}
+    </section>
+</template>
+
+<script>
+export default {
+    props:['card']
+}
+</script>
+
+<style>
+
+</style>
