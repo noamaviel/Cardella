e8953eca19bb76b5859c2c243fda1677c4db811c
Noam Aviel <noamaviel@gmail.com>
2020-11-25 17:06:20 +0200

    Board Service

---
+++ src/services/board-service.js
@@ -0,0 +1,75 @@
+import HttpService from './http-service'
+
+export default {
+    getBoards,
+    getBoardById,
+    addBoard,
+    remove,
+    update
+}
+
+function getBoards() {
+    return HttpService.get('boards')
+}
+function getBoardById(boardId) {
+    return HttpService.get(`boards/${boardId}`)
+}
+
+function addBoard(board) {
+    return HttpService.post('boards', board)
+}
+function remove(boardId) {
+    return HttpService.delete(`boards/${boardId}`)
+}
+
+function update(board) {
+    return HttpService.put(`boards/${board._id}`, board)
+}
+
+
+// Service Testing
+
+
+// // var testboard = {_id: "b102", test:'testboard'}
+// // update(testboard);
+
+// // async function update(board) {
+// //   console.log('board in ENTRANCE', board)
+// //   var t = await boardService.update(board)
+// //   console.log('updatedboard', t)
+// // }
+
+// getBoards();
+
+// async function getBoards() {
+//   var bs = await boardService.getBoards();
+//   console.log('boards:', bs);
+// }
+
+// const testBoard = {
+//   _id: "b101"
+// }
+// getBoardById(testBoard._id)
+
+// async function getBoardById(testBoardId) {
+//   var bst = await boardService.getBoardById(testBoardId);
+//   console.log('boardById', bst)
+// }
+
+// removeBoard("b102")
+
+// async function removeBoard(boardId) {
+//   var rb = await boardService.remove(boardId)
+//   console.log('removedBoard', rb)
+// }
+
+// // const newTestBoard = {
+// //   _id: "b102"
+// // }
+
+// // addBoard(newTestBoard)
+
+// // async function addBoard(newBoard) {
+// //   var ntb = await boardService.addBoard(newBoard);
+// //   console.log('newBoard', ntb);
+// // }
---
+++ src/services/http-service.js
@@ -0,0 +1,47 @@
+import router from '../router/router.js'
+// import store from '../store/store.js'
+
+const BASE_URL = process.env.NODE_ENV === 'production'
+    ? '/api/'
+    : '//localhost:3000/'
+
+
+import Axios from 'axios';
+var axios = Axios.create({
+    withCredentials: true
+});
+
+export default {
+    get(endpoint, data){
+        console.log('endpoint', endpoint);
+        return ajax(endpoint, 'GET', data)
+    },
+    post(endpoint, data){
+        return ajax(endpoint, 'POST', data)
+    },
+    put(endpoint, data){
+        return ajax(endpoint, 'PUT', data)
+    },
+    delete(endpoint, data){
+        return ajax(endpoint, 'DELETE', data)
+    }
+}
+
+
+async function ajax(endpoint, method='get', data=null) {
+    try {
+        const res = await axios({
+            url: `${BASE_URL}${endpoint}`,
+            method,
+            data
+        })
+        return res.data;
+    } catch (err) {
+        if (err.response.status === 401) {
+            router.push('/');
+        }
+        console.log(`Had issues ${method}ing to server`, err)
+        throw err;
+    }
+}
+
