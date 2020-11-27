d1a277045dd1c6ee5ebeb31ca178f54fd33b0d80
Mor Lev Ari <morshva@gmail.com>
2020-11-27 17:44:22 +0200

    side menu

--- src/cmps/board/board-activity-log.cmp.vue
+++ src/cmps/board/board-activity-log.cmp.vue
@@ -1,9 +1,51 @@
-
 <template>
-  <section>
-    <h4>Activity list</h4>
-    <!-- <ul class="activity-list">
-      <li></li>
-    </ul> -->
+  <section class="activities-container">
+    <div class="activities">
+      <div class="activity" v-for="activity in activities" :key="activity.id">
+        <div class="activity-content">
+          <p>
+            <!-- <avatar
+              class="avatar"
+              :src="activity.createdBy.imgUrl"
+              :username="activity.createdBy.fullName"
+              :lighten="100"
+              :size="30"
+              background-color="#f15656"
+              color="#172b4d"
+            /> -->
+            <span class="name">{{ activity.createdBy.fullname }}</span>
+            <span> left a comment: </span>
+            <!-- <span v-if="activity.isComment">left a comment:</span> -->
+            <!-- <span v-if="isShowInCard">{{ activity.text | removeInCard }}</span> -->
+            <span>{{ activity.txt }}</span>
+          </p>
+          <small class="time">{{ activity.createdAt | timeSince }}</small>
+        </div>
+      </div>
+    </div>
   </section>
 </template>
+
+<script>
+import moment from "moment";
+// import Avatar from "vue-avatar";
+export default {
+  props: {
+    activities: Array,
+    // members: Array,
+  },
+  // computed: {
+  //   activitiesToShow() {
+  //     return this.activities.slice(0, 20);
+  //   },
+  // },
+  filters: {
+    timeSince(timestamp) {
+      return moment(timestamp).fromNow();
+    },
+  },
+  components: {
+    // Avatar,
+  },
+};
+</script>
--- src/cmps/board/side-menu.cmp.vue
+++ src/cmps/board/side-menu.cmp.vue
@@ -1,11 +1,21 @@
 <template>
   <section class="side-menu">
-    <ul class="menu-actions">
-      <li>Change background</li>
-      <li>Delete board</li>
-      <h4>Activity Log:</h4>
-      <board-activity-log />
-    </ul>
+    <div class="side-menu-header">
+      <h3>Menu</h3>
+    </div>
+    <div class="side-menu-nav">
+      <h5><i class="far fa-trash-alt"></i> Delete Board</h5>
+      <h5><i class="fas fa-palette"></i> Change Background</h5>
+    </div>
+    <div class="activity-title">
+      <h5><i class="fas fa-list-ul"></i> Activity Log:</h5>
+    </div>
+    <div class="side-activities">
+      <board-activity-log
+        class="activity-preview"
+        :activities="board.boardActivities"
+      />
+    </div>
   </section>
 </template>
 
@@ -18,10 +28,6 @@ export default {
   },
   computed: {},
   methods: {
-    getActivities() {
-      //     this.
-      // }
-    },
     created() {},
   },
   components: {
@@ -29,3 +35,5 @@ export default {
   },
 };
 </script>
+
+
