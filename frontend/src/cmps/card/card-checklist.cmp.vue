9d34f094732d179ebc73c1620d66192a25037f3a
Shimrit Herbst <shimrit.levine@gmail.com>
2020-12-06 15:20:30 +0200

    checklist fixing

--- frontend/src/cmps/card/card-checklist.cmp.vue
+++ frontend/src/cmps/card/card-checklist.cmp.vue
@@ -1,70 +1,95 @@
 <template>
-  <section class="card-checklist">
-    <div class="edit-card-checklist flex">
-      <h3 class="edit-card-title"><i class="el-icon-finished"></i></h3>
-      <h3
-        class="checklist-title flex"
-        contenteditable="true"
-        @keypress.enter.prevent="updateChecklistTitle"
-        @blur="updateChecklistTitle"
-      >
-        {{ checklist.title }}
-      </h3>
-    </div>
-    <section class="progress-bar-container flex f-row">
-      <h5 class="progress-bar-title">
-        {{ getPercentage }}
-      </h5>
-      <div class="progress-bar flex">
-        <div
-          class="progress-bar-percent"
-          :style="{ width: getPercentage }"
-        ></div>
-      </div>
-    </section>
-    <div class="checklist-list-container">
-      <ul v-if="checklist.todos">
-        <li class="flex"
-          v-for="(todo, index) in checklist.todos"
-          :index="index"
-          :key="todo.id"
+    <section class="card-checklist">
+        <div class="edit-card-checklist flex">
+            <h3 class="edit-card-title"><i class="el-icon-finished"></i></h3>
+            <h3
+                class="checklist-title flex"
+                contenteditable="true"
+                @keypress.enter.prevent="updateChecklistTitle"
+                @blur="updateChecklistTitle"
+            >
+                {{ checklist.title }}
+            </h3>
+        </div>
+        <section class="progress-bar-container flex f-row">
+            <h5 class="progress-bar-title">
+                {{ getPercentage }}
+            </h5>
+            <div class="progress-bar flex">
+                <div
+                    class="progress-bar-percent"
+                    :style="{ width: getPercentage }"
+                ></div>
+            </div>
+        </section>
+        <div class="checklist-list-container">
+            <ul v-if="checklist.todos">
+                <li
+                    class="flex"
+                    v-for="(todo, index) in checklist.todos"
+                    :index="index"
+                    :key="todo.id"
+                >
+                    <button
+                        class="checklist-btn-remove-todo"
+                        @click="onRemoveTodo(index)"
+                    >
+                        <i class="el-icon-close"></i>
+                    </button>
+
+                    <label
+                        v-if="todo.isDone"
+                        :style="{ 'text-decoration': 'line-through' }"
+                    >
+                        <input
+                            class="checkbox"
+                            type="checkbox"
+                            v-model="todo.isDone"
+                            @change="onCheckbox"
+                        />
+                        {{ todo.title }}
+                    </label>
+
+                    <label v-else :style="{ 'text-decoration': 'none' }">
+                        <input
+                            class="checkbox"
+                            type="checkbox"
+                            v-model="todo.isDone"
+                            @change="onCheckbox"
+                        />
+                        {{ todo.title }}
+                    </label>
+                </li>
+            </ul>
+        </div>
+
+        <button
+            class="checklist-btn-add-item clr-btn"
+            v-if="!isAddTodoForm"
+            @click="onAddTodoForm"
         >
-          <button class="checklist-btn-remove-todo" @click="onRemoveTodo(index)">
-            <i class="el-icon-close"></i>
-          </button>
-
-          <label
-            v-if="todo.isDone"
-            :style="{ 'text-decoration': 'line-through' }"
-          >
-            <input class="checkbox" type="checkbox" v-model="todo.isDone" @change="onCheckbox" />
-            {{ todo.title }}
-          </label>
-
-          <label v-else :style="{ 'text-decoration': 'none' }">
-            <input class="checkbox" type="checkbox" v-model="todo.isDone" @change="onCheckbox" />
-            {{ todo.title }}
-          </label>
-        </li>
-      </ul>
-    </div>
-    
-    <button class="checklist-btn-add-item clr-btn" v-if="!isAddTodoForm" @click="onAddTodoForm">
-      Add an item
-    </button>
-    <form class="add-todo-item-container flex" v-if="isAddTodoForm" @submit.prevent="onAddTodo">
-      <input
-        class="checklist-input"
-        type="text"
-        placeholder="Add an item"
-        v-model="tmpTodo.title"
-      />
-      <button class="checklist-btn-add-todo clr-btn">Add</button>
-      <button class="checklist-btn close-add-todo close" @click="onCloseAddTodo">
-        <i class="el-icon-close"></i>
-      </button>
-    </form>
-  </section>
+            Add an item
+        </button>
+        <form
+            class="add-todo-item-container flex"
+            v-if="isAddTodoForm"
+            @submit.prevent="onAddTodo"
+        >
+            <input
+                class="checklist-input"
+                type="text"
+                placeholder="Add an item"
+                v-model="tmpTodo.title"
+            />
+            <button class="checklist-btn-add-todo clr-btn">Add</button>
+            <button
+                class="checklist-btn close-add-todo close"
+                @click="onCloseAddTodo"
+            >
+                <i class="el-icon-close"></i>
+            </button>
+        </form>
+    </section>
 </template>
 
 <script>
@@ -91,7 +116,7 @@ export default {
                 0
             );
             if (doneCount === 0) {
-                return "0";
+                return "0%";
             } else {
                 return (
                     ((doneCount / this.checklist.todos.length) * 100).toFixed(
